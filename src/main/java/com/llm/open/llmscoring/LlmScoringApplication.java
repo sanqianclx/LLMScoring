14b2b856934f35e7611f03754954a3ea350083ea
三千 <147899359+sanqianclx@users.noreply.github.com>
2026-04-06 12:30:17 +0800

    init

---
+++ src/main/java/com/llm/open/llmscoring/LlmScoringApplication.java
@@ -0,0 +1,13 @@
+package com.llm.open.llmscoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmScoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LlmScoringApplication.class, args);
+    }
+
+}
